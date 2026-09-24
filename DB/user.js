f3024345f36f95a0ce82a38b89f7a4778d116037
putraarga2245-droid <putraarga2245@gmail.com>
2026-09-24 09:17:52 +0700

    Create User model with email and createdAt fields

---
+++ DB/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true, 
+    lowercase: true 
+  },
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
+  }
+});
+
+module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
